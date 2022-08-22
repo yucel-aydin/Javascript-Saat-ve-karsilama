c8a185f8865d1b679503cd5d2c99c6ee2fdc19b1
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-08-22 17:26:00 +0300

    Proje için istenenler eklendi

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,33 @@
-let userName=window.prompt("Lütfen adınızı giriniz...");
+let userName = window.prompt("Lütfen adınızı giriniz...");
 if (userName) {
-    document.getElementById("myName").innerText=userName;
+  document.getElementById("myName").innerText = userName;
 }
+
+var myClock = document.getElementById("myClock");
+
+function showTime() {
+  var days = [
+    "Pazartesi",
+    "Salı",
+    "Çarşamba",
+    "Perşembe",
+    "Cuma",
+    "Cumartesi",
+    "Pazar",
+  ];
+
+  var d = new Date();
+  var s = d.getSeconds();
+  var m = d.getMinutes();
+  var h = d.getHours();
+  m = checkTime(m);
+  s = checkTime(s);
+  myClock.innerText = h + ":" + m + ":" + s + " " + days[d.getDay()-1];
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+setInterval(showTime, 1000);
